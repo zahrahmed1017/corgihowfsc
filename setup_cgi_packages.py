60b1edbcc65e61543892852e93d6321a5ab9c86c
Zahra Ahmed <zahra.ahmed1017@gmail.com>
2026-07-10 17:05:41 -0700

    Updating setup_cgi_packages to the latest version of Proper (3.3.5)

--- setup_cgi_packages.py
+++ setup_cgi_packages.py
@@ -121,8 +121,8 @@ def main():
     packages = [
         {
             'name': 'Proper',
-            'zip': 'proper_v3.3.4_python.zip',
-            'dir': 'proper_v3.3.4_python',
+            'zip': 'proper_v3.3.5_python.zip',
+            'dir': 'proper_v3.3.5_python',
             'url': 'https://sourceforge.net/projects/proper-library/'
         },
         {
